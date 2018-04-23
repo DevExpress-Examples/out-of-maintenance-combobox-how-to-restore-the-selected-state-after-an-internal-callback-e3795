72d15af0fb274a6fd9355b9656ebff7b921d3f62
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MvcApp_Q367963/Scripts/JScript1.js
@@ -0,0 +1,10 @@
+﻿var comboBox_value
+
+function Button_OnClick(s, e) {
+    comboBox_value = combo.GetValue();
+    combo.PerformCallback();
+}
+
+function ComboBox_OnEndCallback(s, e) {
+    s.SetValue(comboBox_value);
+}
